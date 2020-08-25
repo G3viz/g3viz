7c0b1b193be708892cf0e67b7a6c8310728fe1b7
phoeguo <phoeguo@gmail.com>
2020-08-24 18:25:23 -0700

    version 1.1.3

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("g3viz: visualizing gene/genome/gentics data for fun.Pfam (v33.1)  UniProt (date: 2020/08/24)\nAny questions, please send emails to <g3viz.group@gmail.com> or post on GitHub <https://github.com/G3viz/g3viz/issues>.")
+  packageStartupMessage("g3viz: visualizing gene/genome/gentics data for fun.\nPfam (v33.1)  UniProt (date: 2020/08/24)\nAny questions, please send emails to <g3viz.group@gmail.com> or post on GitHub <https://github.com/G3viz/g3viz/issues>.")
 }
