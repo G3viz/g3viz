f303f99732e51b91c76783842a16c4101d7cf05a
Xin Guo <phoeguo@gmail.com>
2024-07-29 01:44:31 -0700

    v1.2.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname){
   packageStartupMessage(
 "g3viz: visualizing gene/genome/gentics data for fun.
-  - Pfam (v33.1)
-  - UniProt (date: 2022/07/01)\n
+  - Pfam (v37.0)
+  - UniProt (date: 2024/07/29)\n
 Any questions, please send emails to <g3viz.group@gmail.com> or post on GitHub <https://github.com/G3viz/g3viz/issues>."
     )
 }
