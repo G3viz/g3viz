9aa2cf8dd371b8580031791b7d436b802efba6ab
Xin Guo <phoeguo@gmail.com>
2024-09-03 15:27:18 -0700

    v1.2.0

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
 "g3viz: visualizing gene/genome/gentics data for fun.
   - Pfam (v37.0)
   - UniProt (date: 2024/07/29)\n
+Check documentation at GitHub pages <https://g3viz.github.io/g3viz/>
 Any questions, please send emails to <g3viz.group@gmail.com> or post on GitHub <https://github.com/G3viz/g3viz/issues>."
     )
 }
